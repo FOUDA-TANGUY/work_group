a6bc4a62006b1b62ee64923b5494ac6d5161e009
cedric <andzamac12@gmail.com>
2023-12-18 19:06:58 +0100

    prototypes

--- head.h
+++ head.h
@@ -1,6 +1,10 @@
 #ifndef HEAD__H__
 #define HEAD__H__
 
+    void addition(int , int);
+    void mutiplication(int , int);
+    void division(int , int);
+    void soustration(int , int);
     // prototype de fonction as ecrrirr
 
 #endif
