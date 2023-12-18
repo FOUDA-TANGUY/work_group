d248c0ece0c8d6b112b3d8d3a0241f311cc2d332
TANGUY <blondelle.fouda@facsciences-uy1.cm>
2023-12-18 19:22:08 +0100

    ..

--- main.c
+++ main.c
@@ -3,17 +3,32 @@
 int main()
 {
     int choix;
+    int a,b;
     do
     {
         printf("########################################\n");
-        print("\t 1 -> add\n");
-        print("\t 2 -> sous\n");
-        print("\t 3 -> mul\n");
-        print("\t 4 -> div\n");
-        print("\t 0 -> QUIT\n");
+        printf("\t 1 -> add\n");
+        printf("\t 2 -> sous\n");
+        printf("\t 3 -> mul\n");
+        printf("\t 4 -> div\n");
+        printf("\t 0 -> QUIT\n");
         printf("\n faites un choix !!");
         scanf("%d",&choix);
-
+        if( choix != 0)
+        {
+            printf("entrez les deux nombre a et b\n");
+            scanf("%d %d",&a,&b);
+        }
+        switch (choix)
+        {
+        case 1:
+            printf("")
+            /* code */
+            break;
+        
+        default:
+            break;
+        }
     }while(choix != 0);
     
 
