c307664b8e727096de03df3073b64a54a0d378c2
cedric <andzamac12@gmail.com>
2023-12-18 19:12:59 +0100

    fonctios

--- fonction.c
+++ fonction.c
@@ -1,2 +1,28 @@
 #include <stdio.h>
 #include "head.h"
+
+int addition(int a , int b)
+{
+    int som;
+    som = a+b;
+    return som;
+}
+int multiplication(int a , int b)
+{
+    int pro;
+    prod = a*b;
+    return pro;
+}
+int soustration(int a , int b)
+{
+    int sous;
+    sous = a-b;
+    return sous;
+}
+int division(int a , int b)
+{
+    int div;
+    if(b!=0)
+        div = a/b;
+    return div;
+}
--- head.h
+++ head.h
@@ -1,10 +1,10 @@
 #ifndef HEAD__H__
 #define HEAD__H__
 
-    void addition(int , int);
-    void mutiplication(int , int);
-    void division(int , int);
-    void soustration(int , int);
+    int addition(int , int);
+    int mutiplication(int , int);
+    int division(int , int);
+    int soustration(int , int);
     // prototype de fonction as ecrrirr
 
 #endif
