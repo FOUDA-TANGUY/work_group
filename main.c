c0180f7dd4c1fad989a7ac7c8c3f8aaec33c5d9d
TANGUY <blondelle.fouda@facsciences-uy1.cm>
2023-12-18 19:12:51 +0100

    main set

--- main.c
+++ main.c
@@ -2,6 +2,20 @@
 #include "head.h"
 int main()
 {
-    printf("Hello word\n");
+    int choix;
+    do
+    {
+        printf("########################################\n");
+        print("\t 1 -> add\n");
+        print("\t 2 -> sous\n");
+        print("\t 3 -> mul\n");
+        print("\t 4 -> div\n");
+        print("\t 0 -> QUIT\n");
+        printf("\n faites un choix !!");
+        scanf("%d",&choix);
+
+    }while(choix != 0);
+    
+
     return 0;
 }
